361dfda26c2499ee3902d600584993fade5e6d43
Petr Pichugin <petr.pichugin@gmail.com>
2023-02-11 15:51:44 +0500

    3.1.2 digits counter with channel

---
+++ 03_Concurrency/3.1.2_withchannels/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+// counter хранит количество цифр в каждом слове.
+// ключ карты - слово, а значение - количество цифр в слове.
+type counter map[string]int
+
+// countDigitsInWords считает количество цифр в словах фразы
+func countDigitsInWords(phrase string) counter {
+	words := strings.Fields(phrase)
+	counted := make(chan int)
+
+	// начало решения
+
+	go func() {
+		for _, word := range words {
+			counted <- countDigits(word)
+		}
+		close(counted)
+	}()
+
+	// Считайте значения из канала counted
+	// и заполните stats.
+
+	stats := make(map[string]int, len(phrase))
+	for _, word := range words {
+		digits := <-counted
+		stats[word] = digits
+	}
+
+	// В результате stats должна содержать слова
+	// и количество цифр в каждом.
+
+	// конец решения
+
+	return stats
+}
+
+// countDigits возвращает количество цифр в строке
+func countDigits(str string) int {
+	count := 0
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			count++
+		}
+	}
+	return count
+}
+
+// printStats печатает слова и количество цифр в каждом
+func printStats(stats counter) {
+	for word, count := range stats {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
+
+func main() {
+	phrase := "0ne 1wo thr33 4068"
+	stats := countDigitsInWords(phrase)
+	printStats(stats)
+}
