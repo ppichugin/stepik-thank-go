6c1e004cab646ec9ae3b0215304bdc807deb8c9e
Petr Pichugin <petr.pichugin@gmail.com>
2023-02-17 14:35:22 +0500

    3.6.2 cancel by context for all functions

---
+++ 03_Concurrency/3.6_context/3.6.2_conveyorwithcontext/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+// информация о количестве цифр в каждом слове
+type counter map[string]int
+
+// слово и количество цифр в нем
+type pair struct {
+	word  string
+	count int
+}
+
+// начало решения
+
+// считает количество цифр в словах
+func countDigitsInWords(ctx context.Context, words []string) counter {
+	pending := submitWords(ctx, words)
+	counted := countWords(ctx, pending)
+	return fillStats(ctx, counted)
+}
+
+// отправляет слова на подсчет
+func submitWords(ctx context.Context, words []string) <-chan string {
+	out := make(chan string)
+	go func() {
+		defer close(out)
+		for _, word := range words {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- word:
+			}
+		}
+	}()
+	return out
+}
+
+// считает цифры в словах
+func countWords(ctx context.Context, in <-chan string) <-chan pair {
+	out := make(chan pair)
+	go func() {
+		defer close(out)
+		for word := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- newPair(word):
+			}
+		}
+	}()
+	return out
+}
+
+// создает новую пару. Применяется для предотвращения утечки горутин.
+func newPair(word string) pair {
+	return pair{word, countDigits(word)}
+}
+
+// готовит итоговую статистику
+func fillStats(ctx context.Context, in <-chan pair) counter {
+	stats := counter{}
+	for p := range in {
+		select {
+		case <-ctx.Done():
+			return stats
+		default:
+			stats[p.word] = p.count
+		}
+	}
+	return stats
+}
+
+// конец решения
+
+// считает количество цифр в слове
+func countDigits(str string) int {
+	count := 0
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	phrase := "0ne 1wo thr33 4068"
+	words := strings.Fields(phrase)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	stats := countDigitsInWords(ctx, words)
+	fmt.Println(stats)
+}
