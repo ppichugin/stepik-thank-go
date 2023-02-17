14900a1a49b138e60feca142a7469b507c863865
Petr Pichugin <petr.pichugin@gmail.com>
2023-02-17 16:58:01 +0500

    3.7.4 map with sync.RWMutex

---
+++ 03_Concurrency/3.7_sync/3.7.4_safemap2/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// начало решения
+
+type Counter struct {
+	lock sync.RWMutex
+	m    map[string]int
+}
+
+func (c *Counter) Increment(str string) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.m[str]++
+}
+
+func (c *Counter) Value(str string) int {
+	c.lock.RLock()
+	i := c.m[str]
+	c.lock.RUnlock()
+
+	return i
+}
+
+func (c *Counter) Range(fn func(key string, val int)) {
+	c.lock.RLock()
+	for s, i := range c.m {
+		fn(s, i)
+	}
+	c.lock.RUnlock()
+}
+
+func NewCounter() *Counter {
+	return &Counter{
+		lock: sync.RWMutex{},
+		m:    map[string]int{},
+	}
+}
+
+// конец решения
+
+func main() {
+	counter := NewCounter()
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	increment := func(key string, val int) {
+		defer wg.Done()
+		for ; val > 0; val-- {
+			counter.Increment(key)
+		}
+	}
+
+	go increment("one", 100)
+	go increment("two", 200)
+	go increment("three", 300)
+
+	wg.Wait()
+
+	fmt.Println("two:", counter.Value("two"))
+
+	fmt.Print("{ ")
+	counter.Range(func(key string, val int) {
+		fmt.Printf("%s:%d ", key, val)
+	})
+	fmt.Println("}")
+}
