4b3866a253c4ba7559ccab4f96bd6923771e3f25
Petr Pichugin <petr.pichugin@gmail.com>
2023-03-02 20:29:37 +0500

    4.5.2 json custom unmarshal

---
+++ 04_Stdlib/4.5_jsonxmlcsv/4.5.2_filmfromjson/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// начало решения
+
+// Genre описывает жанр фильма
+type Genre string
+
+func (g *Genre) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+
+	var temp struct {
+		Name string `json:"name"`
+	}
+	err := json.Unmarshal(data, &temp)
+	if err != nil {
+		return err
+	}
+	*g = Genre(temp.Name)
+	return nil
+}
+
+// Movie описывает фильм
+type Movie struct {
+	Title  string  `json:"name"`
+	Year   int     `json:"released_at"`
+	Genres []Genre `json:"tags"`
+}
+
+// конец решения
+
+func main() {
+	const src = `{
+		"name": "Interstellar",
+		"released_at": 2014,
+		"director": "Christopher Nolan",
+		"tags": [
+			{ "name": "Adventure" },
+			{ "name": "Drama" },
+			{ "name": "Science Fiction" }
+		],
+		"duration": "2h49m",
+		"rating": "★★★★★"
+	}`
+
+	var m Movie
+	err := json.Unmarshal([]byte(src), &m)
+	fmt.Println(err)
+	// nil
+	fmt.Println(m)
+	// {Interstellar 2014 [Adventure Drama Science Fiction]}
+}
