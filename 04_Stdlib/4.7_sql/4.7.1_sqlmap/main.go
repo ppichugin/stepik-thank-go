0810961381e345594b190261e3a1fdfd9311a448
Petr Pichugin <petr.pichugin@gmail.com>
2023-03-11 20:07:13 +0500

    4.7.1 sqlmap on SQLite

---
+++ 04_Stdlib/4.7_sql/4.7.1_sqlmap/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// начало решения
+
+// SQLMap представляет карту, которая хранится в SQL-базе данных
+type SQLMap struct {
+	key     string
+	val     any
+	connect *sql.DB
+}
+
+// NewSQLMap создает новую SQL-карту в указанной базе
+func NewSQLMap(db *sql.DB) (*SQLMap, error) {
+	query := `create table if not exists map(key text primary key, val blob)`
+	_, err := db.Exec(query)
+	if err != nil {
+		return nil, err
+	}
+	return &SQLMap{connect: db}, nil
+}
+
+// Get возвращает значение для указанного ключа.
+// Если такого ключа нет - возвращает ошибку sql.ErrNoRows.
+func (m *SQLMap) Get(key string) (any, error) {
+	query := `select val from map where key = ?`
+	row := m.connect.QueryRow(query, key)
+	err := row.Scan(&m.val)
+	if err == sql.ErrNoRows {
+		return nil, sql.ErrNoRows
+	} else if err != nil {
+		return nil, err
+	}
+	return m.val, nil
+}
+
+// Set устанавливает значение для указанного ключа.
+// Если такой ключ уже есть - затирает старое значение (это не считается ошибкой).
+func (m *SQLMap) Set(key string, val any) error {
+	query := `insert into map(key, val) values (?, ?) on conflict (key) do update set val = excluded.val`
+	_, err := m.connect.Exec(query, key, val)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete удаляет запись карты с указанным ключом.
+// Если такого ключа нет - ничего не делает (это не считается ошибкой).
+func (m *SQLMap) Delete(key string) error {
+	query := `delete from map where key = ?`
+	_, err := m.connect.Exec(query, key)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// конец решения
+
+func main() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	m, err := NewSQLMap(db)
+	if err != nil {
+		panic(err)
+	}
+
+	m.Set("name", "Alice")
+	m.Set("age", 42)
+
+	name, err := m.Get("name")
+	fmt.Printf("name = %v, err = %v\n", name, err)
+	// name = Alice, err = <nil>
+
+	age, err := m.Get("age")
+	fmt.Printf("age = %v, err = %v\n", age, err)
+	// age = 42, err = <nil>
+
+	m.Set("name", "Bob")
+	name, err = m.Get("name")
+	fmt.Printf("name = %v, err = %v\n", name, err)
+	// name = Bob, err = <nil>
+
+	m.Delete("name")
+	name, err = m.Get("name")
+	fmt.Printf("name = %v, err = %v\n", name, err)
+	// name = <nil>, err = sql: no rows in result set
+}
